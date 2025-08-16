1a8cc592a8d96ae9dfb2b1814b8617e1d75e33b7
Bhargavi-04407 <bhargumunvalli@gmail.com>
2025-08-16 22:07:17 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative_Ai_Project',
+    version='0.0.0',
+    author='Bhargavi Munavalli',
+    author_email='bhargumunvalli@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
